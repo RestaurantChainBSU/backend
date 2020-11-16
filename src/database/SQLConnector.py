6d71f8c909a1d5a72a75971f9ba0e3b76739c80f
SanyaRx <alex100zx@gmail.com>
2020-11-16 21:11:15 +0300

    add connection to db

---
+++ src/database/SQLConnector.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from mysql.connector.constants import ClientFlag
+import os
+
+config = {
+    'user': 'admin',
+    'password': 's1a2n3y4a5',
+    'host': '35.223.243.32',
+    'database': 'rzfm_app',
+    'client_flags': [ClientFlag.SSL],
+    'ssl_ca': '/app/src/database/ssl/server-ca.pem',
+    'ssl_cert': '/app/src/database/ssl/client-cert.pem',
+    'ssl_key': '/app/src/database/ssl/client-key.pem'
+}
+
+class SQLConnector:
+    def __init__(self):
+        cwd = os.getcwd()
+        self.cnxn = mysql.connector.connect(**config)
+        self.cursor = self.cnxn.cursor()
+
+
+    def execute_query(self, command):
+        self.cursor.execute(command)
+        return self.cursor
+
+
+    def execute_insert_update(self, command):
+        self.cursor.execute(command)
+        self.cnxn.commit()
+        return self.cursor
