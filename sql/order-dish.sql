6c74d83d48838c53fd2a90b72fb5079cf5ad3eed
Rai Alexander <rai-alexander@users.noreply.github.com>
2020-11-19 23:10:55 +0300

    add order dish table

--- sql/order-dish.sql
+++ sql/order-dish.sql
@@ -1,1 +1,11 @@
 use rzfm_app;
+
+drop table if exists ORDER_DISH;
+
+create table ORDER_DISH(
+	id int primary key auto_increment,
+    order_id int not null,
+    dish_id int not null,
+    foreign key(order_id) references `ORDER`(id),
+	foreign key(dish_id) references DISH(id)
+);
