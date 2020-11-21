3ab94dac4d3572b046b85a36d27c1d909552ce98
dimafurs1337 <dimafurs1337@gmail.com>
2020-11-21 20:36:12 +0300

    Solve CORS issue

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from src.database import SQLConnector
 from flask_restplus import Api, Resource, fields
 from src.api import api
+from flask_cors import CORS
 
 flask_app = Flask(__name__)
+CORS(app)
 api.init_app(flask_app)
 
 if __name__ == "__main__":
