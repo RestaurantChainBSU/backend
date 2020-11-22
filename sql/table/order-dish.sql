2773ce1baa53f6ea943e6dd9f15e729937fd227e
Rai Alexander <rai-alexander@users.noreply.github.com>
2020-11-22 21:39:21 +0300

    change database tables

--- sql/table/order-dish.sql
+++ sql/table/order-dish.sql
@@ -6,6 +6,7 @@ create table ORDER_DISH(
 	id int primary key auto_increment,
     order_id int not null,
     dish_id int not null,
-    foreign key(order_id) references `ORDER`(id),
-	foreign key(dish_id) references DISH(id)
+    amount int not null,
+    foreign key(order_id) references `ORDER`(id) on delete cascade,
+	foreign key(dish_id) references DISH(id) on delete cascade
 );
